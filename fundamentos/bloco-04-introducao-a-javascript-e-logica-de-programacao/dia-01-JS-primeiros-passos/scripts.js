c75eac72762f018db37766c885a68c2eb00165d8
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-02-02 22:58:41 -0300

    Exercicios feitos ate o 6

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-JS-primeiros-passos/scripts.js
@@ -0,0 +1,95 @@
+// Primeiro Exercício
+// // const a = 3
+// // const b = 4
+
+// // console.log('Soma:  ' + (a+b));
+// // console.log('Subtração:  ' + (a-b));
+// // console.log('Multiplicação:  ' + (a*b));
+// // console.log('Divisão:  ' + (a/b));
+// // console.log('Módulo:  ' + (a%b));
+
+
+// Segundo Exercício
+// // const a = 3
+// // const b = 4
+
+// // if( a > b ){
+// //     console.log ('a é maior que b');}
+// // else
+// //     console.log ('a é menor que b')
+// // }
+
+
+// Terceiro Exercício
+// // const a = 9
+// // const b = 8
+// // const c = 10
+
+// //  if(a > b && a > c){
+// //      console.log('O maior número é a')
+// //  }
+// //  else if(b > a && b > c){
+// //     console.log('O maior número é b')
+// // }
+// // else {
+// //     console.log('O maior número é c')
+// // }
+
+
+// Quarto Exercício
+// // const a = 0
+// // const b = 4
+
+// // if( a >= 0){
+// //     console.log ('Positive')
+// // }
+// // else 
+// // console.log ('Negative')
+
+
+// // Quinto Exercício
+// let degreeAngleA = 65;
+// let degreeAngleB = 100;
+// let degreeAngleC = 15;
+
+// let sumOfAngles = degreeAngleA + degreeAngleB + degreeAngleC;
+
+// let allAnglesArePositives = degreeAngleA > 0 && degreeAngleB > 0 && degreeAngleC > 0;
+
+// if(allAnglesArePositives){
+//   if (sumOfAngles === 180) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   };
+// } else {
+//   console.log('Erro: ângulo inválido');
+// }
+
+
+// // Sexto Exercício
+// const peça = 'bispo';
+
+// switch (peça.toLocaleLowerCase()) {
+//     case 'rei':
+//         console.log('Rei -> Uma casa para qualquer direção')
+//     break
+//     case 'rainha':
+//         console.log('Rainha -> Diagonal, horizontal, vertical')
+//     break
+//     case 'bispo':
+//         console.log('Bispo -> Diagonais')
+//     break
+//     case 'cavalo':
+//         console.log('Cavalo -> Movimenta em "L", pode pular sobre as peças ')
+//     break
+//     case 'torre':
+//         console.log('Torre -> Horizontal e vertical')
+//     break
+//     case 'peão':
+//         console.log('Peão -> Vertical')
+//     break
+//     default:
+//         console.log('Peça inválida')
+//         break;
+// }
