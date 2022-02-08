4f03c5f1e0e2ecb73cc37b219ed6541f6c840928
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-02-08 00:34:24 -0300

    Adiciona for in e numeros romanos

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-JS-primeiros-passos/scripts/forIn-forOf.js
@@ -0,0 +1,84 @@
+let espetinho = {
+    sabor : "Carne",
+    preco: 6.00,
+    farofa: true
+};
+
+
+// for (let key in espetinho) {
+//     console.log(espetinho[key]);
+// }
+
+
+// for (let key in espetinho) {
+//     console.log(key);
+// }
+
+
+// for (let key in espetinho) {
+//     console.log(key, espetinho[key]);
+// }
+
+
+// let car = {
+//     type: 'Fiat',
+//     model: '500',
+//     color: 'white',
+//   };
+  
+//   for (let key in car) {
+//     console.log(key, car[key]);
+//   }
+
+
+// let food = ['hamburguer', 'bife', 'acarajé'];
+// for (let value of food) {
+//   console.log(value);
+// };
+//resultado: hamburguer, bife, acarajé;
+
+
+
+// let names = {
+//     person1: 'João',
+//     person2: 'Maria',
+//     person3: 'Jorge',
+//   };
+//   for(let index in names) {
+//       console.log("Olá", names[index]);
+// }
+
+
+
+// let car = {
+//     model: 'A3 Sedan',
+//     manufacturer: 'Audi',
+//     year: 2020
+//   };
+// // for(let index in car) {
+// //     console.log("Modelo:", car.model, "Marca:", car.manufacturer, "Ano:", car.year);
+// // }
+
+// for(let index in car) {
+//     console.log(index,": ", car[index]);
+// }
+
+
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+  
+console.log("Bem-Vinda,", info.personagem);
+info.recorrente = 'Sim';
+console.log(info)
+
+// for (let index in info){
+//     console.log(index)
+// }
+
+// for (let index in info){
+//     console.log(info[index])
+// }
