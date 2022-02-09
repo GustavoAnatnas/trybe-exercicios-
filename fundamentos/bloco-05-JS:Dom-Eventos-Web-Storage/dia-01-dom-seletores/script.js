154c5a3f4cc6d86d0c32f826fd62ed8c8b3aa227
Gustavo Sant'Ana <gssgustavogss@gmail.com>
2022-02-09 18:35:50 -0300

    Exercicios 5.1

---
+++ fundamentos/bloco-05-JS:Dom-Eventos-Web-Storage/dia-01-dom-seletores/script.js
@@ -0,0 +1,13 @@
+const paragraph = document.getElementById("paragraph");
+      paragraph.style.color = "red";
+      document.getElementById("page-title").innerText = "filme favorito"; 
+      document.getElementById("second-paragraph").innerText = "testetestetstetstetstetstets"
+      document.getElementById("subtitle").innerText = "funciona?funciona?"
+
+                       
+      document.getElementsByClassName("para")[0].innerText = "olá, teste"; //modifica conteudo de um unico elemento da classe determinada, no caso a posição do paragrafo é 0. Então indice [0]
+      document.getElementsByTagName("h4")[0].innerText = "não sei o que digitar" // pega nome da div, no caso H4, e altera o conteudo dentro dela 
+
+
+
+
